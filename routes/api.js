f832a36599d1cea707ab40ed4dddf45921777423
cahangeorge <cahangeorge@gmail.com>
2021-03-22 21:47:53 +0200

    finish the project

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,33 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    if(initNum === 'invalid number'){
+      res.json('invalid number');
+    }
+
+    if(initUnit === 'invalid unit'){
+      res.json('invalid unit');
+    }
+
+    if(initNum === 'invalid number' && initUnit === 'invalid unit'){
+      res.json('invalid number and unit');
+    }
+
+    let responseObject = {};
+    responseObject['initNum'] = initNum;
+    responseObject['initUnit'] = initUnit;
+    responseObject['returnNum'] = returnNum;
+    responseObject['returnUnit'] = returnUnit;
+    responseObject['toString'] = toString;
+
+    res.json(responseObject);
+  });
 };
